2b3891a61c8d9ce6e9c5234f5ebef8df61b705e5
human <ex@example.com>
2020-10-28 14:12:57 +0300

    Добавлен класс Point и функция distance

--- sandbox/src/main/java/ru/stqa/pft/sandbox/hello.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/hello.java
@@ -10,10 +10,19 @@ public class hello {
 
     Rectangle r = new Rectangle(4, 6);
     System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+    Point p1 = new Point(2,3);
+    Point p2 = new Point(4,5);
+    System.out.println("Расстояние между двумя точками (" + p1.x + "," + p1.y + ")" + " и " + "(" + p2.x + "," + p2.y + ")" + " = " + distance(p1, p2));
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double distance(Point p1, Point p2) {
+    double sqrt = Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
+    return sqrt;
   }
 
 }
