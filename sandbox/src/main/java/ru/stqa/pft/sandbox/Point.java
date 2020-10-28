0d44f2983df4c39b4463d5e77ba8ade3ff0dd090
human <ex@example.com>
2020-10-28 15:02:30 +0300

    Добавлен метод distance в класс Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -10,4 +10,9 @@ public class Point {
     this.y = y;
   }
 
+  public double distance(Point p2) {
+    double sqrt = Math.sqrt(Math.pow((p2.x - this.x), 2) + Math.pow((p2.y - this.y), 2));
+    return sqrt;
+  }
+
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/hello.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/hello.java
@@ -13,16 +13,17 @@ public class hello {
 
     Point p1 = new Point(2,3);
     Point p2 = new Point(4,5);
-    System.out.println("Расстояние между двумя точками (" + p1.x + "," + p1.y + ")" + " и " + "(" + p2.x + "," + p2.y + ")" + " = " + distance(p1, p2));
+    System.out.println("Расстояние между двумя точками (" + p1.x + "," + p1.y + ")" + " и " + "(" + p2.x + "," + p2.y + ")" + " = " + p1.distance(p2));
+
+
+    Point p3 = new Point(15,27);
+    Point p4 = new Point(36,84);
+    System.out.println("Расстояние между двумя точками (" + p3.x + "," + p3.y + ")" + " и " + "(" + p4.x + "," + p4.y + ")" + " = " + p3.distance(p4));
+
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
-  }
-
-  public static double distance(Point p1, Point p2) {
-    double sqrt = Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
-    return sqrt;
   }
 
 }
