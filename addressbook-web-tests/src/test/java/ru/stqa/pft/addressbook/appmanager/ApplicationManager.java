46318be059f01cf566f216913a9937a8bedcf0f4
human <ex@example.com>
2020-11-07 15:38:05 +0300

    Создан класс GroupHelper, методы по работе с группами перенесены в него, кроме gotoGroup, наследование в ApplicationManager заменено на делегирование, создан конструктор в GroupHelper и другие изменения в коде, связанные с делегированием

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -6,17 +6,19 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.stqa.pft.addressbook.model.ContactData;
-import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
   public WebDriver wd;
 
+  private GroupHelper groupHelper;
+
   public void init() {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook");
+    groupHelper = new GroupHelper(wd);
     login("admin", "secret");
   }
 
@@ -26,28 +28,6 @@ public class ApplicationManager {
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.id("LoginForm")).submit();
-  }
-
-  public void returnToGroupPage() {
-    wd.findElement(By.linkText("groups")).click();
-  }
-
-  public void submitGroupCreation() {
-    wd.findElement(By.name("submit")).click();
-  }
-
-  public void fillGroupForm(GroupData groupData) {
-    wd.findElement(By.name("group_name")).click();
-    wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
-    wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-    wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-  }
-
-  public void initGroupCreation() {
-    wd.findElement(By.name("new")).click();
   }
 
   public void gotoGroupPage() {
@@ -74,14 +54,6 @@ public class ApplicationManager {
     } catch (NoAlertPresentException e) {
       return false;
     }
-  }
-
-  public void deleteSelectedGroups() {
-    wd.findElement(By.name("delete")).click();
-  }
-
-  public void selectGroup() {
-    wd.findElement(By.name("selected[]")).click();
   }
 
   public void submitContactCreation() {
@@ -111,5 +83,9 @@ public class ApplicationManager {
 
   public void initContactCreation() {
     wd.findElement(By.linkText("add new")).click();
+  }
+
+  public GroupHelper getGroupHelper() {
+    return groupHelper;
   }
 }
