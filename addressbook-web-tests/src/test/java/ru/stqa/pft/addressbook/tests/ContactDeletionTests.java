d08a691f9d7a7b5474a123972be46e311e9eb284
human <ex@example.com>
2020-11-14 14:38:45 +0300

    Релизована проверка и обеспечение выполнения предусловий в тестах создания и модификации контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -1,11 +1,23 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
 
 public class ContactDeletionTests extends TestBase {
 
   @Test
   public void testContactDeletion() {
+    if (! app.getContactHelper().isThereAContact()) {
+      app.getContactHelper().createContact(new ContactData(
+              "test1",
+              "test",
+              "test",
+              "test",
+              "test",
+              "test",
+              "test"), true);
+    }
+    app.getNavigationHelper().gotoHomePage();
     app.getContactHelper().selectContact();
     app.getContactHelper().deleteSelectedContacts();
   }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -7,17 +7,27 @@ public class ContactModificationTests extends TestBase {
 
   @Test
   public void testContactModification() {
+    if (! app.getContactHelper().isThereAContact()) {
+      app.getContactHelper().createContact(new ContactData(
+        "test1",
+        "test",
+        "test",
+        "test",
+        "test",
+        "test",
+        "test"), true);
+    }
+    app.getNavigationHelper().gotoHomePage();
     app.getContactHelper().selectContact();
     app.getContactHelper().initContactModification();
     app.getContactHelper().fillContactForm(new ContactData(
-            null,
-            "Petr",
-            "Petrov",
-            "Home int.",
-            "4951234567",
-            "9661234567",
-            "petrov@test.ru"),
-            false
+      null,
+      "Petr",
+      "Petrov",
+      "Home int.",
+      "4951234567",
+      "9661234567",
+      "petrov@test.ru"), false
     );
     app.getContactHelper().submitContactModification();
   }
