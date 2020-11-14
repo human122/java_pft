e45ae76eee5103762d88b145fc99d5b160b9a8d2
human <ex@example.com>
2020-11-14 16:05:09 +0300

    Второе ускорение isPrimeFast

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Primes.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Primes.java
@@ -29,7 +29,8 @@ public class Primes {
   }
 
   public static boolean isPrimeFast(int n) {
-    for (int i = 2; i < n / 2; i++) {
+    int m = (int) Math.sqrt(n);
+    for (int i = 2; i < m; i++) {
       if (n % i == 0) {
         return false;
       }
