3120805d2c9e9575039d61e0b2a35dc7d0dfc1d0
human <ex@example.com>
2020-11-15 18:04:26 +0300

    Получение элементов из списка строк

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.sandbox;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Collections {
@@ -8,13 +9,10 @@ public class Collections {
   public static void main(String[] args) {
     String[] langs = {"Java", "C#", "Python", "PHP"};
 
-    List<String> languages = new ArrayList<String>();
-    languages.add("Java");
-    languages.add("C#");
-    languages.add("Python");
+    List<String> languages = Arrays.asList("Java", "C#", "Python", "PHP");
 
-    for (String l : languages) {
-      System.out.println("Я хочу выучить " + l);
+    for (int i = 0; i < languages.size(); i++) {
+      System.out.println("Я хочу выучить " + languages.get(i));
     }
   }
 
