1de5b25130fcdd107ca10b6d9e5576dd59d62320
human <ex@example.com>
2020-11-15 16:32:59 +0300

    Добавлен другой способ обойти массив в цикле for

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
@@ -6,8 +6,8 @@ public class Collections {
 
     String[] langs = {"Java", "C#", "Python", "PHP"};
 
-    for (int i = 0; i < langs.length; i++) {
-      System.out.println("Я хочу выучить " + langs[i]);
+    for (String l : langs) {
+      System.out.println("Я хочу выучить " + l);
     }
   }
 
