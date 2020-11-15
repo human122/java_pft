78716e801f0680ec14d41c475b754a1cd2751475
human <ex@example.com>
2020-11-15 16:38:25 +0300

    Добавлен интерфейс List и одна из реализаций интерфейся ArrayList

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
@@ -1,12 +1,19 @@
 package ru.stqa.pft.sandbox;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Collections {
 
   public static void main(String[] args) {
-
     String[] langs = {"Java", "C#", "Python", "PHP"};
 
-    for (String l : langs) {
+    List<String> languages = new ArrayList<String>();
+    languages.add("Java");
+    languages.add("C#");
+    languages.add("Python");
+
+    for (String l : languages) {
       System.out.println("Я хочу выучить " + l);
     }
   }
