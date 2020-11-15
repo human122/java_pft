12029f7fb87cb92dddbac548db70699e88216811
human <ex@example.com>
2020-11-15 18:06:35 +0300

    Получение объекта из списка объектов, если тип элементов списка не указан

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
@@ -9,10 +9,10 @@ public class Collections {
   public static void main(String[] args) {
     String[] langs = {"Java", "C#", "Python", "PHP"};
 
-    List<String> languages = Arrays.asList("Java", "C#", "Python", "PHP");
+    List languages = Arrays.asList("Java", "C#", "Python", "PHP");
 
-    for (int i = 0; i < languages.size(); i++) {
-      System.out.println("Я хочу выучить " + languages.get(i));
+    for (Object l : languages) {
+      System.out.println("Я хочу выучить " + l);
     }
   }
 
