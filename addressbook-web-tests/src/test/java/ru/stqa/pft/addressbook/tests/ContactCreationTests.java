f2546f6cd90e97166a3362fd8cb4a0e7f8761d12
human <ex@example.com>
2020-11-22 16:26:55 +0300

    В тесты по созданию групп и контактов добавлена проверка списка элементов до создания группы или контакта и после

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -3,7 +3,9 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class ContactCreationTests extends TestBase {
@@ -12,19 +14,24 @@ public class ContactCreationTests extends TestBase {
   public void testContactCreation() throws Exception {
     List<ContactData> before = app.getContactHelper().getContactList();
     app.getContactHelper().initContactCreation();
-    app.getContactHelper().fillContactForm(new ContactData(
-      "test1",
-      "Petr",
-      "Petrov",
-      "Home int.",
-      "4951234567",
-      "9661234567",
-      "petrov@test.ru"), true
+    ContactData contact = new ContactData(
+            "test1",
+            "Petr",
+            "Petrov",
+            "Home int.",
+            "4951234567",
+            "9661234567",
+            "petrov@test.ru"
     );
+    app.getContactHelper().fillContactForm(contact,true);
     app.getContactHelper().submitContactCreation();
     app.getNavigationHelper().gotoHomePage();
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() + 1);
+
+    before.add(contact);
+    Assert.assertEquals(new HashSet<Object>(after), new HashSet<Object>(before));
+
   }
 
 }
