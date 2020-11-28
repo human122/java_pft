c9d0164e9eb780fd97912d0323db899e5ce8eef7
human <ex@example.com>
2020-11-28 12:43:37 +0300

    Проведена доп. реструктуризация кода тестов модификации и удаления групп и контактов с выделением методов модификации, проверки предусловий и использованием повторящихся частей кода с помощью одной переменной

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -2,6 +2,7 @@ package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.By;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
@@ -9,8 +10,9 @@ import java.util.List;
 
 public class ContactDeletionTests extends TestBase {
 
-  @Test
-  public void testContactDeletion() throws InterruptedException {
+  @BeforeMethod
+  public void ensurePreconditions() {
+    app.getNavigationHelper().gotoHomePage();
     if (! app.getContactHelper().isThereAContact()) {
       app.getContactHelper().createContact(new ContactData(
               "test1",
@@ -22,14 +24,19 @@ public class ContactDeletionTests extends TestBase {
               "test"), true);
     }
     app.getNavigationHelper().gotoHomePage();
+  }
+
+  @Test
+  public void testContactDeletion() throws InterruptedException {
     List<ContactData> before = app.getContactHelper().getContactList();
-    app.getContactHelper().selectContact(before.size() - 1);
+    int index = before.size() - 1;
+    app.getContactHelper().selectContact(index);
     app.getContactHelper().deleteSelectedContacts();
     app.getNavigationHelper().gotoHomePage();
     List<ContactData> after = app.getContactHelper().getContactList();
-    Assert.assertEquals(after.size(), before.size() - 1);
+    Assert.assertEquals(after.size(), index);
 
-    before.remove(before.size() - 1);
+    before.remove(index);
     Assert.assertEquals(before, after);
   }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
@@ -9,23 +10,26 @@ import java.util.List;
 
 public class ContactModificationTests extends TestBase {
 
-  @Test
-  public void testContactModification() {
+  @BeforeMethod
+  public void ensurePreconditions() {
+    app.getNavigationHelper().gotoHomePage();
     if (! app.getContactHelper().isThereAContact()) {
       app.getContactHelper().createContact(new ContactData(
-        "test1",
-        "test",
-        "test",
-        "test",
-        "test",
-        "test",
-        "test"), true);
+              "test1",
+              "test",
+              "test",
+              "test",
+              "test",
+              "test",
+              "test"), true);
     }
     app.getNavigationHelper().gotoHomePage();
+  }
 
+  @Test
+  public void testContactModification() {
     List<ContactData> before = app.getContactHelper().getContactList();
-    app.getContactHelper().selectContact(before.size() - 1);
-    app.getContactHelper().initContactModification(before.size() - 1);
+    int index = before.size() - 1;
     ContactData contact = new ContactData(
             null,
             "Petr",
@@ -34,13 +38,12 @@ public class ContactModificationTests extends TestBase {
             "4951234567",
             "9661234567",
             "petrov@test.ru");
-    app.getContactHelper().fillContactForm(contact, false);
-    app.getContactHelper().submitContactModification();
+    app.getContactHelper().modifyContact(index, contact);
     app.getNavigationHelper().gotoHomePage();
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size());
 
-    before.remove(before.size() - 1);
+    before.remove(index);
     before.add(contact);
     Assert.assertEquals(new HashSet<Object>(after), new HashSet<Object>(before));
   }
