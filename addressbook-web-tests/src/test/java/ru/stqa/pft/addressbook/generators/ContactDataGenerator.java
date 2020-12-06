1f8bc28d7509829f40e05c9f4eff42208eb13381
human <ex@example.com>
2020-12-06 10:58:05 +0300

    Созданы генераторы тестовых данных для групп и контактов в csv

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -0,0 +1,41 @@
+package ru.stqa.pft.addressbook.generators;
+
+import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactDataGenerator {
+
+  public static void main(String[] args) throws IOException {
+    int count = Integer.parseInt(args[0]);
+    File file = new File(args[1]);
+
+    List<ContactData> contacts = generateContacts(count);
+    save(contacts, file);
+  }
+
+  private static void save(List<ContactData> contacts, File file) throws IOException {
+    System.out.println(new File(".").getAbsolutePath());
+    Writer writer = new FileWriter(file);
+    for (ContactData contact : contacts) {
+      writer.write(String.format("%s;%s\n", contact.getFirstname(), contact.getLastname()));
+    }
+    writer.close();
+  }
+
+  private static List<ContactData> generateContacts(int count) {
+    List<ContactData> contacts = new ArrayList<ContactData>();
+    for (int i = 0; i < count; i++) {
+      contacts.add(new ContactData()
+              .withFirstname(String.format("Petr%s", i))
+              .withLastname(String.format("Petrov%s", i))
+      );
+    }
+    return contacts;
+  }
+}
